22a22dbd385f149c507d74e61d5240adca331604
luke robertson <robertson.stuff@gmail.com>
2021-09-20 21:51:21 +1000

    Update Main.js

--- Main.js
+++ Main.js
@@ -16,7 +16,7 @@ var slideIndex = 0;
   	  slides[slideIndex-1].style.display = "block";  
   	  dots[slideIndex-1].className += " active";
   	  setTimeout(showSlides, 3500); // Change image every 3.5 seconds
-	  }
+	}
 	showSlides2();
 
 	function showSlides2() {
